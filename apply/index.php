8bd20e949d75860841a8180d4cef841635cef67f
Anlimah <y.m.ratty7@gmail.com>
2024-10-28 14:39:05 +0000

    Updated foreign applicants link

--- apply/index.php
+++ apply/index.php
@@ -39,7 +39,7 @@ if (!isset($_SESSION["_start"])) {
                                     <span style="color: red; font-weight:bolder"> Link </span>
                                 </a> (
                                 <a href="https://foreign.rmuictonline.com">
-                                    <span style="color: red; font-weight:bolder"> https://forms.rmuictonline.com </span>
+                                    <span style="color: red; font-weight:bolder"> https://foreign.rmuictonline.com </span>
                                 </a>) to purchase an e-voucher using <span style="color:#003262; font-weight:bolder">ONLY</span> Card (Visa & Master).
                             </p>
                         </div>
